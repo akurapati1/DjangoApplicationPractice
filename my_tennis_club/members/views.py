93f57f33abe97499122d2842f1a4b1ffd30cffd8
Abhilash Kurapati <abhilash.kurapati@slu.edu>
2025-03-03 21:15:44 -0600

    Learnt how to send the data in the model to a template through a view

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import members as mb
 
 def members(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers = mb.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers' : mymembers,
+    }
+
+    return HttpResponse(template.render(context, request))
 
 # Create your views here.
