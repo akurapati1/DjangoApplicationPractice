00448f1a15e58589530fc8820e6f614de140771e
Abhilash Kurapati <abhilash.kurapati@slu.edu>
2025-03-03 21:59:03 -0600

    Learnt how to ad master and main templates

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,23 +1,23 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import members as mb
 
 def members(request):
-    mymembers = mb.objects.all().values()
-    template = loader.get_template('all_members.html')
-    context = {
-        'mymembers' : mymembers,
-    }
-
-    return HttpResponse(template.render(context, request))
-
-# Create your views here.
-
+  mymembers = mb.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+  
 def details(request, id):
-    mymember = mb.objects.get(id=id)
-    template = loader.get_template('details.html')
-    context = {
-        'mymember' : mymember,
-    }
-    return HttpResponse(template.render(context, request))
+  mymember = mb.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+  
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
