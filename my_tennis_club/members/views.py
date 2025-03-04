49a33ec7015ee694439363b2e67a7995a0128b1a
Abhilash Kurapati <abhilash.kurapati@slu.edu>
2025-03-03 21:33:53 -0600

    Learnt how to add links inside a template to another template

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -13,3 +13,11 @@ def members(request):
     return HttpResponse(template.render(context, request))
 
 # Create your views here.
+
+def details(request, id):
+    mymember = mb.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember' : mymember,
+    }
+    return HttpResponse(template.render(context, request))
